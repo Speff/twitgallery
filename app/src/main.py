db8f327a719252433b0c621c57471a7ee9a7fd50
Speff <smpatel89@gmail.com>
2019-04-23 18:39:03 -0400

    Initial commit. Created web/app/db containers

---
+++ app/src/main.py
@@ -0,0 +1,11 @@
+import psycopg2
+import config
+from configparser import ConfigParser
+import signal
+
+def main():
+    print("Started")
+    signal.pause()
+
+if __name__ == '__main__':
+    main()
---
+++ db/init.sql
@@ -0,0 +1,5 @@
+CREATE TABLE twitter_posts(
+    created_at TEXT,
+    id TEXT,
+    text TEXT
+);
