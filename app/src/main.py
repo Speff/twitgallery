7bcef62e5328eb266e2d6113ec0d6586eb661717
Speff <smpatel89@gmail.com>
2019-04-23 22:00:00 -0400

    Improved pg lookup for user_status

--- app/src/main.py
+++ app/src/main.py
@@ -12,11 +12,11 @@ class process_user(Resource):
         screen_name = request.form["user_id"]
 
         user_status_result = check_user_status(screen_name)
-        if user_status_result == False:
+        if user_status_result == "db_error":
             user_status = "db connection error"
             status_code = 500
-        elif user_status_result == True:
-            user_status = "db connected"
+        else:
+            user_status = user_status_result
             status_code = 202
 
         return {
@@ -30,10 +30,20 @@ def check_user_status(screen_name):
     try:
         pg_con = psycopg2.connect(pg_connect_info)
     except:
-        return False
+        return "db_error"
     else:
-        pg_con.close()
-        return True
+        pg_cur = pg_con.cursor()
+        pg_cur.execute("""SELECT status FROM user_status WHERE screen_name=%s;""", (screen_name,))
+        user_status = pg_cur.fetchone()
+
+        if user_status == None:
+            pg_cur.execute("""INSERT INTO user_status(screen_name, status) VALUES (%s, 'started')""", (screen_name,))
+            pg_con.commit()
+            pg_con.close()
+            return "started"
+        else:
+            pg_con.close()
+            return user_status[0]
 
 
 if __name__ == '__main__':
