dad791f762697a227ddc2ee9f446b4b47b5f146b
Speff <smpatel89@gmail.com>
2019-05-02 19:59:46 -0400

    added image loading to web frontend

--- web/www/main.js
+++ web/www/main.js
@@ -1,7 +1,24 @@
 function main(){
     $(document).ready(function(){
-        $.post("/api/get_results", {"screen_name": "@speff7"}, function(data, status){
-            $("#target").text(data);
+        $.post("/api/get_results", {"user_id": "@speff7"}, function(data, status){
+            $("#target").append("<div class='post_row'>");
+            $("#target").append("<div class='post_col'>");
+            var n_posted = 0;
+            $.each(data.status, function(index, value){
+                for(var i = 0; i < 4; i++){
+                    if(value["media_url_"+i] != null){
+                        $("#target").append("<img src='"+value["media_url_"+i]+"' class='post'></img>");
+                        n_posted = n_posted + 1;
+                        if(n_posted % 6 == 0){
+                            $("#target").append("</div><div class='post_col'>");
+                        }
+                    }
+                }
+
+                if(index > 10) return false;
+            });
+            $("#target").append("</div>");
+            $("#target").append("</div>");
         }, "json");
     });
 }
