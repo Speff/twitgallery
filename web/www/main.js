1930830e011a318d78a72a948bc8e18823d38d31
Speff <smpatel89@gmail.com>
2019-05-06 13:27:10 -0400

    Add page console line
    Add page console line for debugging on mobile

--- web/www/main.js
+++ web/www/main.js
@@ -121,6 +121,7 @@ $(document).keyup(function(e) {
 
 $(document).on("scroll", function(e){
     var scroll_pos = $(document).scrollTop() + $(window).height();
+    $("#page_console").text(scroll_pos + " / " + $(document).height());
     if($(document).height() - scroll_pos < 200){
         if(query_in_progress == false){
             query_in_progress = true;
