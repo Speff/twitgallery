4c8ffb4777549e623088ad872fb4f0fe3536bc40
Speff <smpatel89@gmail.com>
2019-05-05 21:45:25 -0400

    Changed units on CSS & combined all items on one grid

--- web/www/main.js
+++ web/www/main.js
@@ -19,13 +19,6 @@ function display_images(user){
         }
 
         offset += 1;
-        $("#target").append("<div id='grid_" + offset + "' class='grid'></div>");
-        console.log("packing");
-        $("#grid_1").packery({
-            // options
-            itemSelector: '.grid-item',
-            stagger: 5,
-        });
 
         $.each(data.status, function(index, value){
             for(var i = 0; i < 4; i++){
@@ -35,7 +28,7 @@ function display_images(user){
                     post_html += "<img class='blur_image' src='" + value["media_url_"+i] + ":small'></img>";
                     post_html += "</div>";
                     //$("#grid_"+offset).append(post_html);
-                    $("#grid_1").append(post_html);
+                    $("#grid").append(post_html);
 
                     var new_element = $("#post_"+image_count);
 
@@ -64,7 +57,7 @@ function display_images(user){
                     new_element.attr("data-"+"possibly_sensitive", value["possibly_sensitive"]);
                     new_element.attr("data-"+"image", "<img src='"+value["media_url_"+i]+":large'></img>");
 
-                    $("#grid_1").packery('appended', new_element);
+                    $("#grid").packery('appended', new_element);
 
                     var img_src = new_element.click({"tag": new_element}, display_modal);
                     image_count += 1;
@@ -143,6 +136,15 @@ $(document).ready(function(){
             query_in_progress = true;
 
             var user_to_process = "@" + $("#user_input").text();
+
+            $("#target").append("<div id='grid' class='grid'></div>");
+            console.log("packing");
+            $("#grid").packery({
+                // options
+                itemSelector: '.grid-item',
+                stagger: 5,
+            });
+
             display_images(user_to_process);
         }
     });
