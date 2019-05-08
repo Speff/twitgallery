464cde19bbe1fd012a04220f484f280be9fc6752
Speff <smpatel89@gmail.com>
2019-05-08 15:54:06 -0400

    Removed sleep_on_rate_limit for python script
    Triggering exceptions on rate limits will enable the program to report
    back errors

--- app/src/main.py
+++ app/src/main.py
@@ -14,8 +14,7 @@ api = Api(app)
 twit_api = twitter.Api(consumer_key=os.environ['CONSUMER_KEY'],
                   consumer_secret=os.environ['CONSUMER_SECRET'],
                   access_token_key=os.environ['ACCESS_TOKEN'],
-                  access_token_secret=os.environ['ACCESS_TOKEN_SECRET'],
-                  sleep_on_rate_limit=True);
+                  access_token_secret=os.environ['ACCESS_TOKEN_SECRET'])
 
 pg_connect_info = "dbname=twitgallery user=tg_user password=docker host=db"
 
