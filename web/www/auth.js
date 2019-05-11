a33f7369f3a297431973d4bbcfdd293b4bcd7916
Speff <smpatel89@gmail.com>
2019-05-11 02:21:54 -0400

    Styled auth page

--- web/www/auth.js
+++ web/www/auth.js
@@ -9,14 +9,14 @@ $(document).ready(function(){
     if(searchParams.has('oauth_token') && searchParams.has('oauth_verifier')){
         $.get("/api/auth_twit" + twit_params, function(data){
             if(data.status = "Success")
-                $("#signed_in_message").text("Signed in successfully");
+                $("#signed_in_message").html("Signed in successfully.<br><span class='hr_line'></span><br>This window will close automagically");
             else
-                $("#signed_in_message").text("Sign in failed");
+                $("#signed_in_message").html("Sign in failed<br><span class='hr_line'></span>");
 
             setTimeout(close_window, 2000);
         });
     }
     else
-        $("#signed_in_message").text("Sign in failed");
+        $("#signed_in_message").html("Sign in failed<br><span class='hr_line'></span>");
 });
 
