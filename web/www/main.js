371e202a67d2188e33b702c4b939bf95a086ca04
Speff <smpatel89@gmail.com>
2019-05-14 22:38:12 -0400

    Enable mobile blur

--- web/www/main.js
+++ web/www/main.js
@@ -72,7 +72,7 @@ function display_images(user){
                     post_html += "<p class='loading_message'>Loading...</p>";
                     post_html += "<img class='main_image cover' src='" + value["media_url_"+i].replace("http:", "https:") + ":small'></img>";
 
-                    if(is_mobile);
+                    if(false);
                     else
                         post_html += "<img class='blur_image' src='" + value["media_url_"+i].replace("http:", "https:") + ":small'></img>";
 
